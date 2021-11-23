2bc65f0741e942e4058305f5f79f4e18902225bc
nykfeng <nykfengcode@gmail.com>
2021-11-23 00:36:58 -0500

    Added a To-do List App

---
+++ ToDoList/toDoList.js
@@ -0,0 +1,19 @@
+const addBtn = document.querySelector(".add-btn");
+const listBody = document.querySelector(".body-field");
+const inputField = document.getElementById("toDoInput");
+
+addBtn.addEventListener("click", function () {
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("Para-Style");
+  const removeBtn = document.createElement("i");
+  removeBtn.classList.add("remove-btn", "fas", "fa-trash-alt");
+  paragraph.innerText = inputField.value;
+  listBody.appendChild(paragraph);
+  listBody.appendChild(removeBtn);
+
+  inputField.value = "";
+  removeBtn.addEventListener("click", function () {
+    listBody.removeChild(paragraph);
+    listBody.removeChild(removeBtn);
+  });
+});
